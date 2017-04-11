775a584dda8c1691963c0667e61e7881d83816db
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2017-04-11 22:21:16 +0200

    advanced demo updates

---
+++ advanced-demo/README.md
@@ -0,0 +1,11 @@
+# tree-shaking-advanced-demo
+
+A simple JavaScript file using lodash-es.
+
+This file imports only two functions of lodash, one for templating, one for math addition...
+
+Try to find in dist JavaScript file these functions : function max(
+
+- rollup build does a great job, by only importing what you need.
+
+- webpack too.
--- advanced-demo/src/main.js
+++ advanced-demo/src/main.js
@@ -1,5 +1,11 @@
+/**
+ * Only imports what you need
+ */
 import template from 'lodash-es/template';
 import add from 'lodash-es/add';
+
+// Or test with the big import...
+//import * as lodash from 'lodash-es';
 
 const compiled = template( '<h1 class="<%-className%>"><%=title%></h1>' );
 const addition = add(5,5); //createMathOperation
@@ -8,9 +14,3 @@ console.log(addition, compiled({
     className: 'demo',
     title: 'tree-shaking'
 }));
-
-/*
-import * as lodash from 'lodash-es';
-const compiled = lodash.template( '<h1 class="<%-className%>"><%=title%></h1>' );
-const addition = lodash.add(5,5);
-*/
